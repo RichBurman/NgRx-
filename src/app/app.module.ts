e7e26b744bf1971856a05848eb5dec3d491807d9
Richard Burman <richard.burman@harperjames.co.uk>
2024-07-08 10:25:05 +0100

    Install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { CounterControlsComponent } from './counter-controls/counter-controls.co
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
